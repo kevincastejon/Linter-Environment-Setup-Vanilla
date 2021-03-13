eb974bcab397d90b4a2d28dfa5ba4c23fe124add
Kevin Castejon <contact@kevincastejon.fr>
2021-03-13 11:25:59 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  ignorePatterns: [".eslintignore", "**/vendor/*.js"],
+  rules: {
+    "strict": "off",
+    "linebreak-style": "off",
+    "spaced-comment": "off",
+    "no-console": "off",
+    "prefer-const": "off",
+    "eol-last": "off",
+    "no-plusplus": "off",
+    "no-empty": "warn",
+    "vars-on-top": "off"
+  },
+};
